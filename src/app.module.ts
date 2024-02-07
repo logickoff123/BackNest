c3407fad928bde05251f869a795f313f277de618
Logickoff <SonyaUstinova@gmail.com>
2024-02-07 11:17:33 +0300

    env + conf

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { getPostgresConfig } from './configs/postgress.config';
+import { PromoModule } from './promo/promo.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getPostgresConfig,
+    }),
+    PromoModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
